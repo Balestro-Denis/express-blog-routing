2809cb6901e9b02b6980c3d7b2fd20a46ef87bea
Balestro-Denis <denisbalestro@gmail.com>
2026-03-03 16:16:52 +0100

    Add JSON routes

--- routers/posts.js
+++ routers/posts.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const posts = require("../data/posts");
 
 //INDEX
 router.get("/", (req, res) => {
-  res.send("Lista dei post");
+  res.json(posts);
 });
 
 //SHOW
 router.get("/:id", (req, res) => {
-  res.send("Dettaglio del post" + req.params.id);
+  const id = parseInt(req.params.id);
+
+  const post = posts.find((p) => p.id === id);
+  if (!post) {
+    return res.status(404).json({ error: "Post non trovato" });
+  }
+
+  res.json(post);
 });
 
 //CREATE
