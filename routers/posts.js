af8a1a3a919dd44a90dadffc2851f5db86873a5b
Balestro-Denis <denisbalestro@gmail.com>
2026-03-03 15:56:01 +0100

    Add Crud routers

--- routers/posts.js
+++ routers/posts.js
@@ -1,4 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
+//INDEX
+router.get("/", (req, res) => {
+  res.send("Lista dei post");
+});
+
+//SHOW
+router.get("/:id", (req, res) => {
+  res.send("Dettaglio del post" + req.params.id);
+});
+
+//CREATE
+router.post("/", (req, res) => {
+  res.send("Creazione nuovo post");
+});
+
+//UPDATE
+router.put("/:id", (req, res) => {
+  res.send("Modifica del post" + req.params.id);
+});
+
+//DELETE
+router.delete("/:id", (req, res) => {
+  res.send("Cancellazione del post" + req.params.id);
+});
+
 module.exports = router;
